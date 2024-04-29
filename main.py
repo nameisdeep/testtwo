b4fbf776f5cd01be3d39f39e72d7088061342bac
nameisdeep <97721089+nameisdeep@users.noreply.github.com>
2024-04-29 19:54:17 +0000

    a

--- main.py
+++ main.py
@@ -12,29 +12,7 @@ async def read_root():
     return JSONResponse(content={"Hello": "World"})
 
 
-@app.get("/test", tags=["Root"])
-async def predict(temperature: float, humidity: float, lpg: float):
 
-    new_lpg = ((lpg - 0.1) * (0.016567 - 0.002693) / (10 - 0.1)) + 0.002693 
-
-    new_data_point = [[temperature, humidity, new_lpg]]
-    predicted_cluster = model.predict(new_data_point)
-
-    is_leakage = False
-    if predicted_cluster[0] == 1:
-        if (new_lpg >= 0.003954):
-            is_leakage = True 
-    elif predicted_cluster[0] == 2:
-        if (temperature >= 25.0 and temperature <= 30.9) and (humidity >= 40.0 and humidity <= 60.9) and (lpg >= 0.003954):
-            is_leakage = True
-        elif (temperature >= 31.0 and temperature <= 40.9) and (humidity >= 61.0 and humidity <= 65.9) and (lpg >= 0.004654):
-            is_leakage = True
-        elif (temperature >= 41.0) and (humidity >= 66.0) and (lpg >= 0.005355):
-            is_leakage = True
-    else:
-        is_leakage = False
-    response = {'leakage': 'yes' if is_leakage else 'no'}
-    return response
 
 
 if __name__ == '__main__':
